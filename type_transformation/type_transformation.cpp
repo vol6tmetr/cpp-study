49a54ad3996fc8db1039300fc1e81343276ca954
Vol6tmetr <mythbusterpeter@gmail.com>
2019-05-01 16:14:29 +0300

    Add type_transformation task

---
+++ type_transformation/type_transformation.cpp
@@ -0,0 +1,16 @@
+#include "../std_lib_facilities.h"
+
+int main() {
+  double d = 0;
+
+  while (cin >> d) {
+    int i = d;
+    char c = i;
+    int i2 = c;
+
+    cout << "d = " << d << "\n"
+         << "i = " << i << "\n"
+         << "c = " << c << "\n"
+         << "i2 = " << i2 << "\n";
+  }
+}
