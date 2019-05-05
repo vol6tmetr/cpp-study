be1a0df0eae8982562a4043b7c1c79793d2005c6
Vol6tmetr <mythbusterpeter@gmail.com>
2019-05-05 12:03:55 +0300

    Add numbers comparison in two_numbers_while task

--- two_numbers_while/two_numbers_while.cpp
+++ two_numbers_while/two_numbers_while.cpp
@@ -7,7 +7,15 @@ int main() {
   int number_2 = 0;
 
   while (cin >> number_1 >> number_2) {
-    cout << number_1 << ", " << number_2 << "\n"
-         << "Type in two numbers: ";
+    if (number_1 > number_2) {
+      cout << "Biggest number is " << number_1 << "\n"
+           << "Smallest number is " << number_2 << "\n";
+    } else if (number_2 > number_1) {
+      cout << "Biggest number is " << number_2 << "\n"
+           << "Smallest number is " << number_1 << "\n";
+    } else {
+      cout << "Number " << number_1 << " is equal to " << number_2 << "\n";
+    }
+    cout << "Type in two numbers: ";
   }
 }
