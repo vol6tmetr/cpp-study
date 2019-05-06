48329ef83bbb812954bd98132f205610769c4823
Vol6tmetr <mythbusterpeter@gmail.com>
2019-05-06 09:09:19 +0300

    Add almost identical comparison in two_numbers_while task

--- two_numbers_while/two_numbers_while.cpp
+++ two_numbers_while/two_numbers_while.cpp
@@ -3,8 +3,10 @@
 int main() {
   cout << "Type in two numbers: ";
 
-  int number_1 = 0;
-  int number_2 = 0;
+  constexpr double allowed_difference = 0.01;
+
+  double number_1 = 0;
+  double number_2 = 0;
 
   while (cin >> number_1 >> number_2) {
     if (number_1 > number_2) {
@@ -13,8 +15,13 @@ int main() {
     } else if (number_2 > number_1) {
       cout << "Biggest number is " << number_2 << "\n"
            << "Smallest number is " << number_1 << "\n";
-    } else {
+    } else if (number_1 == number_2) {
       cout << "Number " << number_1 << " is equal to " << number_2 << "\n";
+    }
+
+    if (abs(number_1 - number_2) < allowed_difference) {
+      cout << "Numbers " << number_1 << " and "
+           << number_2 << " are almost identical\n";
     }
     cout << "Type in two numbers: ";
   }
